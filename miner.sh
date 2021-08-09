3981f4012491f80ed3acce536408d1a5f58820be
perisky <feysen@gmail.com>
2021-08-09 19:56:43 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,1 +1,1 @@
-/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u wallet.workername -p x -t 4
+/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RHqxV5XUix1NDz1HVBGFYudeDATvtYmNvM.P5KYL4B209 -p x -t 4
